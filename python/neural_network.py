4363074cfa4d76fb9b6cbb092896e5ed0f4b7aa7
Vincent B <37360448+vincbeaulieu@users.noreply.github.com>
2022-01-05 02:15:03 -0500

    Quick Review

--- python/neural_network.py
+++ python/neural_network.py
@@ -1,12 +1,14 @@
 
-
+import matplotlib as plt
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 
+# bpRNA: large-scale automated annotation and analysis of RNA secondary structure
+# https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6009582/
+
 def neural_network():
 
-    seq_length = 130
     seq_amount = 16384
 
     # Extract Data
@@ -18,16 +20,21 @@ def neural_network():
             for line in st_file:
                 lines.append(line)
 
+        # TODO : Check for #Warnings on line 3, if first char is not 'G', then add one line
+
+        # Extract specific lines from '.st' file
         nt_seq = lines[3] #nucleotides sequences
         db_not = lines[4] #dot-parens (dot-brackets) notation
         knots = lines[5]
         loops = lines[6]
 
+        # Extract into array
         nt_node = list(nt_seq)
         db_node = list(db_not)
         kt_node = list(knots)
         lp_node = list(loops)
 
+        # Remove '\n' at the end
         nt_node.pop()
         db_node.pop()
         kt_node.pop()
@@ -38,7 +45,7 @@ def neural_network():
         # print(kt_node)
         # print(lp_node)
 
-        # TODO : Feed data to Neural Network
+        # TODO : Feed data to 'Mixed Data' Neural Network
 
         gen_index += 1
 
