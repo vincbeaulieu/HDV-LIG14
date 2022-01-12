727c8fe9bb99fd14f49013f7b51ad406f742d214
Vincent B <37360448+vincbeaulieu@users.noreply.github.com>
2022-01-12 01:09:51 -0500

    Encoder Done - Multi-Threading Done

--- python/neural_network.py
+++ python/neural_network.py
@@ -1,5 +1,3 @@
-
-from pandas.core.indexes import category
 import data as dt
 import matplotlib as plt
 import numpy as np
@@ -7,10 +5,13 @@ import pandas as pd
 import tensorflow as tf
 import os
 
+import concurrent.futures as ccf
+
 # bpRNA: large-scale automated annotation and analysis of RNA secondary structure
 # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6009582/
 
 def neural_network():
+    ## Feature Extractions and Encoding
 
     seq_amount = dt.HDV_LIG14.seq_amount
 
@@ -76,74 +77,78 @@ def neural_network():
     # * The neural network for the LIG sequences will be implemented in the
     # * future when all the data will be regenerated using the correct indexing.
 
-    # One Hot Encode Data
-    def one_hot_encoder(sequence,categories,scale=None,remove_last=True):
-        scale = (scale,len(categories))[scale==None]
-        mapping = dict(zip(categories, range(scale)))    
-        onehotseq = [mapping[i] for i in sequence]
-        output = np.rot90(np.eye(scale)[onehotseq])
-        return (output,output[:-1])[remove_last]
-
-    # TODO: open-bracket is +1, close-braket is -1, dots is +0, in a hot format
-    def incremental_hot_encoder(sequence,categories,scales,remove_last=True):
-        # scales, categories: [increment, decrement, constant]
-        map_incr = dict(zip(categories[0], range(scales[0])))
-        map_decr = dict(zip(categories[1], range(scales[1])))
-        map_const = dict(zip(categories[2], range(scales[2])))
-
-        # counters:
-        square = 0  # []
-        round = 0   # ()
-        curly = 0   # {}
-        angle = 0   # <>
-
-        
-
-        # TODO: To be continued...
-
-        pass
-
+    # Encoded Arrays
     nt_encoded = []
     db_encoded = []
     kt_encoded = []
     lp_encoded = []
+
+    # One Hot Encoder
+    def one_hot_encoder(sequence,categories,scale=None,remove_last=True):
+        scale = (scale,len(categories))[scale==None]
+        mapping = dict(zip(categories, range(scale)))
+        results = [mapping[i] for i in sequence]
+        output = np.rot90(np.eye(scale)[results])
+        return (output,output[:-1])[remove_last]
+
+    # Dot-Bracket Encoder
+    def dot_bracket_encoder(sequence,categories=None):
+
+        # Four pairs of brackets
+        categories = ["(.)","[.]","{.}","<.>"]
+
+        # open-bracket is +1, close-braket is -1, dots is +0
+        values = [1.0, 0.0, -1.0]
+
+        # Bracket Mapping
+        round_map = dict(zip(categories[0], values))
+        square_map = dict(zip(categories[1], values))
+        curly_map = dict(zip(categories[2], values))
+        angle_map = dict(zip(categories[3], values))
+
+        # Populate the categories
+        R = []; S = []; C = []; A = []
+        for i in sequence:
+            R.append(round_map.get(i,0))
+            S.append(square_map.get(i,0))
+            C.append(curly_map.get(i,0))
+            A.append(angle_map.get(i,0))
+   
+        # Stack the 4 categories
+        results = np.stack((R,S,C,A),axis=0)
+        return results
     
-    i = 0
-    while i < seq_amount:
-        # NT Categories: ['A','T/U','C','G']
-        for hot in one_hot_encoder(dt.HDV_LIG14.genotypes[i],"ATCG"):
-            nt_encoded.append(hot)
+    
+    ExceptionHandler = []
+    def encoder_thread(encoded_file,encoded_list,encoder,sequences,categories=None):
+        try:
+            i = 0
+            while i < len(sequences):
+                for hot in encoder(sequences[i],categories):
+                    encoded_list.append(hot)
+                    print("\rSEQUENCE_" + str(i) + " -- Done -- ", end='')
+                i += 1
+            dataset_to_csv(encoded_file,encoded_list)
+            return encoded_list
+        except Exception as e:
+            ExceptionHandler.append(e)
 
-        # DB Categories: [['{','(','<','['],[']','>',')','}'],['.']]
-        # TODO: 
-        #   The encoding of Dot-Bracket will be a sort of incremental-hot encoding.
-        #   Where open-bracket is +1, close-braket is -1, dots is +0
-        categories = [['{','(','<','['],[']','>',')','}'],['.']]
-        incremental_hot_encoder(db_dataset[i],categories,[4,4,1])
-
-        # KT Categories: ['B','E','H','I','M','S','X']
-        for hot in one_hot_encoder(kt_dataset[i],"BEHIMSX"):
-            kt_encoded.append(hot)
-
-        # LP Categories: ['N','K']
-        lp_encoded.append(one_hot_encoder(lp_dataset[i],"NK")[0])
-
-        i += 1
-
-    dataset_to_csv('csv/encoded/nt_encoded.csv',nt_encoded)
-    dataset_to_csv('csv/encoded/db_encoded.csv',db_encoded)
-    dataset_to_csv('csv/encoded/kt_encoded.csv',kt_encoded)
-    dataset_to_csv('csv/encoded/lp_encoded.csv',lp_encoded)
+    # Encode Data (Multi-threaded)
+    with ccf.ThreadPoolExecutor() as executor:
+            nt_task = executor.submit(encoder_thread, 'csv/encoded/nt_encoded.csv', nt_encoded, one_hot_encoder, dt.HDV_LIG14.genotypes, "ATCG")
+            db_task = executor.submit(encoder_thread, 'csv/encoded/db_encoded.csv', db_encoded, dot_bracket_encoder, db_dataset)
+            kt_task = executor.submit(encoder_thread, 'csv/encoded/kt_encoded.csv', kt_encoded, one_hot_encoder, kt_dataset, "BEHIMSX")
+            lp_task = executor.submit(encoder_thread, 'csv/encoded/lp_encoded.csv', lp_encoded, one_hot_encoder, lp_dataset, "NK")   
+    
+    # Raise Exception if any
+    for e in ExceptionHandler: raise(e) 
 
     # Multi-Threading:
     # https://www.tutorialspoint.com/python/python_multithreading.htm
 
-    # Feature Extractions and Encoding
+    # NOTE:
     # * The dataset may have been wrongly generated due to an error in the instruction guide. Regeneration of the dataset will retake place using the previously written SPOT-RNA auto generator program, available in the "git" folder. An additional flag may be added to extract even more features. Extraction will take place on both the HDV-Lib14-RNA and LIG-Lib14-RNA sequences. Therefore, the size of the datasets will at least double.
-    # * Dot-bracket encoder is not completed, see "TODO" sections.
-    # * Nucleotide encoder may be further improved by using the corresponding IUPAC nt. This may be something to explore.
-    # * Implement multi-threading!
-
+    # * Nucleotide encoder may further be improved by using the corresponding IUPAC nt. This may be something to explore.
 
     # TODO : Feed data to 'Mixed Data' Neural Network
 
