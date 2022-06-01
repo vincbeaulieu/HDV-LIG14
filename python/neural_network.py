b80af225c3e4845e304b1523adebcd2ecfa4e7fe
Vincent B <37360448+vincbeaulieu@users.noreply.github.com>
2022-06-01 17:44:42 -0400

    Rank System
    evaluate() : This approach will use a rank system and extract the data based on the accuracy of the prediction.
    
    Data will be selected based on a threshold eg: data with >95% accuracy (or based on how many data point the ML can keep). Selected data will be ranked together (or assigned a class). The remaining data will be used to train a different ML. Rinse and repeat until all data is ranked (or until a maximum # of ML is achieve)

--- python/neural_network.py
+++ python/neural_network.py
@@ -23,66 +23,68 @@ def neural_network():
     lines = toolbox.reader('csv/encoded/nt_encoded.csv')
     nt_flat = toolbox.merge(lines,3)
     nt_flat = toolbox.dataset_to_csv('csv/flat/nt_flat.csv',nt_flat)
+    nt_size = len(nt_flat.axes[1])
 
     db_flat = []
     lines = toolbox.reader('csv/encoded/db_encoded.csv')
     db_flat = toolbox.merge(lines,4)
     db_flat = toolbox.dataset_to_csv('csv/flat/db_flat.csv',db_flat)
+    db_size = len(db_flat.axes[1])
 
     kt_flat = []
     lines = toolbox.reader('csv/encoded/kt_encoded.csv')
     kt_flat = toolbox.merge(lines,6)
     kt_flat = toolbox.dataset_to_csv('csv/flat/kt_flat.csv',kt_flat)
-    
+    kt_size = len(kt_flat.axes[1])
+
     lp_flat = []
     lines = toolbox.reader('csv/encoded/lp_encoded.csv')
     lp_flat = toolbox.merge(lines,1)
     lp_flat = toolbox.dataset_to_csv('csv/flat/lp_flat.csv',lp_flat)
+    lp_size = len(lp_flat.axes[1])
 
-    hdv_flat = [hdv_fit, hdv_del]
+    hdv_flat = [hdv_fit, hdv_del, lig_fit, lig_del]
     hdv_flat = np.transpose(hdv_flat)
     hdv_flat = toolbox.dataset_to_csv('csv/flat/hdv_flat.csv',hdv_flat)
 
-    # TODO: fixing error:
-    # ValueError: Failed to convert a NumPy array to a Tensor (Unsupported object type float).
-
-    nt_flat = np.asarray(nt_flat).astype(np.int_)
+    dataframe = nt_flat
+    #dataframe = pd.concat([nt_flat,db_flat,kt_flat,lp_flat], axis=1)
+    dataframe_size = len(dataframe.axes[1])
+    print(dataframe)
+    print(dataframe_size)
+    dataframe = np.asarray(dataframe).astype(np.byte)
+    print(dataframe)
     hdv_flat = np.asarray(hdv_flat).astype('float32')
     
     # ANN for NT Data
     from sklearn.model_selection import train_test_split
-    x_train, x_test, y_train, y_test = train_test_split(nt_flat, hdv_flat, test_size=1/10, random_state=0)
+    x_train, x_test, y_train, y_test = train_test_split(dataframe, hdv_flat, test_size=1/10, random_state=0)
     # NOTE: 10-fold crossvalidation may be implemented 
 
-    from sklearn.preprocessing import StandardScaler
-    scaler = StandardScaler()
-    #x_train = scaler.fit_transform(x_train)
-    #x_test = scaler.transform(x_test)
-    #y_train = scaler.fit_transform(x_train)
-    #y_test = scaler.transform(x_test)
-
-
-    ## ann stand for Artificial Neural Network
+    ## Artificial Neural Network
     model = tfk.models.Sequential()
+    
+
+    input_units = dataframe_size
+    output_units = 4
+    hidden_units = (input_units + output_units) * (2/3)
 
     # Nucleotide input layer
-    model.add(tfk.layers.Dense(units=42, activation='relu'))
+    model.add(tfk.layers.Dense(units=input_units, activation='relu'))
 
     # Hidden layers
-    model.add(tfk.layers.Dense(units=168, activation='relu'))
-    model.add(tfk.layers.Dense(units=84, activation='relu'))
-    model.add(tfk.layers.Dense(units=42, activation='relu'))
-    model.add(tfk.layers.Dense(units=14, activation='relu'))
+    model.add(tfk.layers.Dense(units=hidden_units, activation='relu'))
+    model.add(tfk.layers.Dense(units=hidden_units, activation='relu'))
 
     # Output layer
-    model.add(tfk.layers.Dense(units=2, activation='sigmoid'))
+    model.add(tfk.layers.Dense(units=output_units, activation='sigmoid'))
 
     # Generate the ANN
-    model.compile(optimizer = 'adam', loss = 'poisson', metrics = ['accuracy'])
+    model.compile(optimizer = 'Adagrad', loss = 'poisson', metrics = ['accuracy'])
 
     # Feed data to Neural Network
     # NOTE: Lookup 'Mixed Data' Neural Network
-    model.fit(x_train, y_train, batch_size = 1024, epochs = 2000)
+    model.fit(x_train, y_train, batch_size = len(x_train), epochs = 1000)
 
     prediction = model.predict(x_test)
 
@@ -95,10 +97,12 @@ def neural_network():
     toolbox.dataset_to_csv('csv/prediction/prediction.csv',prediction)
     toolbox.dataset_to_csv('csv/prediction/y_test.csv',y_test)
 
-    save(model,'pkl/nt_MachineLearning.pkl')
+    save(model,'pkl/nt_MachineLearning_model.pkl')
     
-    model = load('pkl/nt_MachineLearning.pkl')
+    model = load('pkl/nt_MachineLearning_model.pkl')
     print(model.predict(x_test))
+
+
 
 
 
@@ -121,8 +125,41 @@ def plot():
     
     print(estimated_fitness[:,0])
 
-    # NOTE: reader does not utilise delimiter - TODO
     plt.scatter(estimated_fitness[:,0],actual_fitness[:,0])
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
+    
+    pass
+
+def evaluate():
+    # Import desired outputs
+    hdv_fit = dt.HDV_LIG14.hdv_fitness
+    hdv_del = dt.HDV_LIG14.hdv_delta
+    lig_fit = dt.HDV_LIG14.ligase_fitness
+    lig_del = dt.HDV_LIG14.ligase_delta
+
+    nt_flat = []
+    lines = toolbox.reader('csv/encoded/nt_encoded.csv')
+    nt_flat = toolbox.merge(lines,3)
+    nt_flat = toolbox.dataset_to_csv('csv/flat/nt_flat.csv',nt_flat)
+
+    hdv_flat = [hdv_fit, hdv_del]
+    hdv_flat = np.transpose(hdv_flat)
+    hdv_flat = toolbox.dataset_to_csv('csv/flat/hdv_flat.csv',hdv_flat)
+
+    nt_flat = np.asarray(nt_flat).astype(np.int_)
+    hdv_flat = np.asarray(hdv_flat).astype('float32')
+
+    # Predict the whole set to evaluate the rank of each results.
+    # Knn will be applied to determine a rank on unranked data.
+    model = load('pkl/nt_MachineLearning_model.pkl')
+    pred = model.predict(nt_flat)
+
+    percent_error = abs((pred[:,0] - hdv_flat[:,0]))/hdv_flat[:,0] * 100
+    #toolbox.dataset_to_csv('csv/prediction/percent_error.csv',percent_error)
+
+    plt.scatter(pred[:,0],hdv_flat[:,0])
     plt.xticks([])
     plt.yticks([])
     plt.show()
@@ -133,6 +170,9 @@ def test():
     print("Testing HDV-LIG14 Neural Network...")
 
     # Testing Neural Network
-    # neural_network()
+    #neural_network()
+    evaluate()
     plot()
     pass
+
+test()
