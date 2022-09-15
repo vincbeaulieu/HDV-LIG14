840d074a94ce076eba146f55f9a2dc081237c186
Vincent B <37360448+vincbeaulieu@users.noreply.github.com>
2022-09-15 01:10:51 -0400

    Update donut.py

--- ML/diagram/donut.py
+++ ML/diagram/donut.py
@@ -1,10 +1,16 @@
-#####################################################################
-# Donut Diagram suggested by Dr. Nawwaf Kharma - Concordia University
+##########################################################
+# Title: Donut Diagram
+# Suggestion from Dr. Nawwaf Kharma - Concordia University
 #
 # Author: Vincent Beaulieu
 # Date: 14 September 2022
-#####################################################################
-#####################################################################
+###########################################################
+###########################################################
+#
+# Info: Display in a circular diagram the folding 
+# probabilities of a RNA sequence using a coolwarm colormap
+#
+###########################################################
 
 import networkx as nx
 import matplotlib.pyplot as plt
