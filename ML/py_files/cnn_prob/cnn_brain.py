fc0be031557899a4f4f3aa88c5f8aa4e9933c6ff
Vincent B <37360448+vincbeaulieu@users.noreply.github.com>
2023-02-25 17:29:06 -0500

    update

--- ML/py_files/cnn_prob/cnn_brain.py
+++ ML/py_files/cnn_prob/cnn_brain.py
@@ -1,34 +1,53 @@
 
-from tensorflow import keras
+from tensorflow import keras, distribute
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from keras.models import Sequential
-from keras.optimizers import Adam
 import matplotlib.pyplot as plt
 import numpy as np
+import psutil
+import multiprocessing
+
+import tensorflow as tf
 
 save_path = "ML/py_files/cnn_prob/"
 
+# Get system stats
+nb_cores = psutil.cpu_count()
+nb_threads = multiprocessing.cpu_count()
 
 def cnn():
-    # Create a CNN model
-    model = Sequential([
-        Conv2D(32, (3, 3), activation='relu', input_shape=(129, 64, 1)),
-        MaxPooling2D((2, 2)),
-        Conv2D(64, (3, 3), activation='relu'),
-        MaxPooling2D((2, 2)),
-        Flatten(),
-        Dense(64, activation='relu'),
-        Dense(1, activation='linear')
-    ])
-    
-    # Compile the model
-    model.compile(optimizer='adagrad',
-                loss='poisson',
-                metrics=['accuracy'])
-                    
+    # Create strategy
+    if tf.test.is_gpu_available():
+        strategy = distribute.MirroredStrategy()
+    else:
+        strategy = tf.distribute.OneDeviceStrategy('/cpu:0')
+
+    with strategy.scope():
+        # Create a CNN model
+        model = Sequential([
+            Conv2D(32, (3, 3), activation='relu', input_shape=(129, 64, 1)),
+            MaxPooling2D((2, 2)),
+            Conv2D(64, (3, 3), activation='relu'),
+            MaxPooling2D((2, 2)),
+            Flatten(),
+            Dense(64, activation='relu'),
+            Dense(1, activation='linear')
+        ])
+        
+        # Compile the model
+        model.compile(optimizer='Adam',
+                    loss='mse',
+                    metrics=['accuracy'])
+
     # Return the model
     return model
 
+
+# Define the function to run in parallel
+def fit_model(model, train_X, train_y, val_X, val_y, epochs=10):
+    #with tf.device('/cpu:0'):
+    model.fit(train_X, train_y, validation_data=(val_X, val_y), epochs=epochs)
+    
 
 if __name__ == '__main__':
     # Load the image data and y values
@@ -46,7 +65,12 @@ if __name__ == '__main__':
     model = cnn()
 
     # Fit the model to the training data
-    history = model.fit(train_X, train_y, epochs=10, validation_data=(val_X, val_y))  # batch_size=32
+    # history = model.fit(train_X, train_y, epochs=10, validation_data=(val_X, val_y))  # batch_size=32
+
+    # Run the model.fit function in parallel
+    with multiprocessing.Pool(processes=nb_cores) as pool:
+        results = pool.starmap(fit_model, [(model, train_X, train_y, val_X, val_y)] * nb_cores)
+        # [r.get() for r in results]
 
     # Evaluate the model
     test_loss, test_mae = model.evaluate(test_X, test_y)
--- ML/py_files/main.py
+++ ML/py_files/main.py
@@ -11,22 +11,25 @@ import tensorflow as tf
 import multiprocessing
 import psutil 
 
+# Get system stats
+nb_cores = psutil.cpu_count()
+nb_threads = multiprocessing.cpu_count()
+
 print(tf.__version__)
 print(tf.config.list_physical_devices())
 
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 print("Num CPUs Available: ", len(tf.config.list_physical_devices('CPU')))
-print("Num CPU Cores Available: ", psutil.cpu_count())
-print("Num CPU Threads Available: ", multiprocessing.cpu_count())
+print("Num CPU Cores Available: ", nb_cores)
+print("Num Threads per CPU Core: ",  int(nb_threads/nb_cores))
+print("Num CPU Threads Available: ", nb_threads)
+
 
 physical_devices = tf.config.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 
-def main():
-
-
-
+if __name__ == '__main__':
         
     # toolbox.test()
     # dt.test()
@@ -41,7 +44,3 @@ def main():
     # pe.position_extractor()
 
     pass
-
-
-if __name__ == '__main__':
-    main()
