8a54558a464f3201254f0f6d29e1ebbb4c82b4c6
Vincent B <37360448+vincbeaulieu@users.noreply.github.com>
2023-02-26 23:30:22 -0500

    Added binary classification model

--- ML/py_files/cnn_prob/bin_brain.py
+++ ML/py_files/cnn_prob/bin_brain.py
@@ -4,35 +4,19 @@ from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from keras.models import Sequential
 import matplotlib.pyplot as plt
 import numpy as np
-import psutil
-import multiprocessing
 import tensorflow as tf
+
 import gen_brain as gb
 
-
-def cnn():
-    # Create strategy
-    if tf.test.is_gpu_available():
-        strategy = distribute.MirroredStrategy()
-    else:
-        strategy = tf.distribute.OneDeviceStrategy('/cpu:0')
-
-    with strategy.scope():
-        # Create a CNN model
-        model = Sequential([
-            Conv2D(32, (3, 3), activation='relu', input_shape=(129, 64, 1)),
-            MaxPooling2D((2, 2)),
-            Conv2D(64, (3, 3), activation='relu'),
-            MaxPooling2D((2, 2)),
-            Flatten(),
-            Dense(64, activation='relu'),
-            Dense(1, activation='linear')
-        ])
-        
-        # Compile the model
-        model.compile(optimizer='Adam',
-                    loss='mse',
-                    metrics=['accuracy'])
+# Define the binary classification model
+def binary_classifier():
+    model = Sequential([
+        Dense(64, activation='relu', input_shape=(129, 64, 1)),
+        Dense(32, activation='relu'),
+        Dense(1, activation='sigmoid')
+    ])
+    # Compile the model
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
     # Return the model
     return model
@@ -43,9 +27,9 @@ if __name__ == '__main__':
     X_ori = np.load(gb.save_path + 'images.npy')
     y_ori = np.load(gb.save_path + 'fitness.npy')
 
-    # Create a new copy of X and y where zero values are removed
-    X = X_ori[y_ori != 0]
-    y = y_ori[y_ori != 0]
+    # Create a new copy of y where non-zero values are replaced by 1
+    X = np.array(X_ori)
+    y = np.where(y_ori == 0, 0, 1)
 
     # Reshape the X to have a single channel
     X = X.reshape((-1, X.shape[1], X.shape[2], 1))
@@ -54,17 +38,13 @@ if __name__ == '__main__':
     train_X, val_X, test_X = np.split(X, [int(0.6 * len(X)), int(0.8 * len(X))])
     train_y, val_y, test_y = np.split(y, [int(0.6 * len(y)), int(0.8 * len(y))])
 
-    # Select/Load the model
-    model = cnn()
-    # model = gb.load("cnn_brain.pkl")
+    # Select the model
+    model = binary_classifier()
 
     # Fit the model using multiprocessing
     # gb.run_multiple(gb.fit_model, model, train_X, train_y, val_X, val_y)
     gb.fit_model(model, train_X, train_y, val_X, val_y)
-
-    # Save the trained model
-    gb.save(model, "cnn_brain.pkl")
-
+    
     # Evaluate the model
     test_loss, test_mae = model.evaluate(test_X, test_y)
     print('Test MAE:', test_mae)
---
+++ ML/py_files/cnn_prob/gen_brain.py
@@ -0,0 +1,54 @@
+
+from tensorflow import keras, distribute
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.models import Sequential
+import matplotlib.pyplot as plt
+import numpy as np
+import psutil
+import multiprocessing
+import tensorflow as tf
+import gen_brain as gb
+
+import os
+import pickle as pk
+
+save_path = "ML/py_files/cnn_prob/"
+
+# Get system stats
+nb_cores = psutil.cpu_count()
+nb_threads = multiprocessing.cpu_count()
+
+# Define the function to run in parallel
+def fit_model(model, train_X, train_y, val_X, val_y, epochs=10):
+    # with tf.device('/cpu:0'):
+    model.fit(train_X, train_y, validation_data=(val_X, val_y), epochs=epochs)
+    
+
+# Run the model.fit function in parallel
+def run_multiple(function, *args):
+    with multiprocessing.Pool(processes=nb_cores) as pool:
+        results = pool.starmap(function, [args] * nb_cores)
+    return results
+
+
+# Evaluate prediction accuracy of the model
+def predict(model, test_X, test_y):
+    prediction = model.predict(test_X)
+
+    # Plot the results
+    plt.scatter(prediction, test_y)
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
+
+
+# Save model (file extension: ".pkl")
+def save(model, name, filepath=save_path):
+    os.makedirs(os.path.dirname(filepath + name), exist_ok=True)
+    pk.dump(model, open(filepath + name, 'wb'))
+
+
+# Load model (file extension: ".pkl")
+def load(name, filepath=save_path):
+    model = pk.load(open(filepath + name, 'rb'))
+    return model
